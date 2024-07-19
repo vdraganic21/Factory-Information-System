774c039d066468441e09e1e0917b192e163bc9e0
vdraganic21 <vdraganic21@student.foi.hr>
2024-07-19 22:47:51 +0200

    Added OrganizationSubject data set.

--- Business Layer/OrganizationSubjectService.cs
+++ Business Layer/OrganizationSubjectService.cs
@@ -16,6 +16,21 @@ namespace Business_Layer
             organizationSubjects.Add(new OrganizationSubject("200", "Odjel zavarene konstrukcije"));
             organizationSubjects.Add(new OrganizationSubject("250", "Glavno skladište"));
             organizationSubjects.Add(new OrganizationSubject("300", "Metal Transport d.o.o."));
+            organizationSubjects.Add(new OrganizationSubject("310", "Odjel za održavanje"));
+            organizationSubjects.Add(new OrganizationSubject("320", "Odjel za kvalitetu"));
+            organizationSubjects.Add(new OrganizationSubject("330", "Odjel za nabavu"));
+            organizationSubjects.Add(new OrganizationSubject("3401", "Ljevaonica"));
+            organizationSubjects.Add(new OrganizationSubject("3402", "Talionica"));
+            organizationSubjects.Add(new OrganizationSubject("360", "Odjel za istraživanje i razvoj"));
+            organizationSubjects.Add(new OrganizationSubject("370", "Servis i popravak"));
+            organizationSubjects.Add(new OrganizationSubject("40057", "Metal Design d.o.o."));
+            organizationSubjects.Add(new OrganizationSubject("41023", "AluKonstrukcija d.o.o."));
+            organizationSubjects.Add(new OrganizationSubject("42021", "Messer Croatia"));
+            organizationSubjects.Add(new OrganizationSubject("43099", "Strojar Bistra d.o.o."));
+            organizationSubjects.Add(new OrganizationSubject("44022", "Gumimpex Varaždin d.d."));
+            organizationSubjects.Add(new OrganizationSubject("45011", "Metal Logistics Ltd."));
+            organizationSubjects.Add(new OrganizationSubject("46033", "Hawle Austrija"));
+            organizationSubjects.Add(new OrganizationSubject("47055", "TIS"));
         }
 
         public List<OrganizationSubject> GetAll()
